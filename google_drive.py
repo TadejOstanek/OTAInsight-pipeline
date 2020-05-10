81f41e194549c11103989e815a6db69bee1fbc16
unknown <tadej.ostanek@spitogatos.gr>
2020-05-10 23:27:46 +0300

    google drive connection from python

---
+++ google_drive.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  3 15:29:52 2020
+connect to google drive
+@author: tadej
+"""
+
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+
+drive = GoogleDrive(gauth)
+f = drive.CreateFile({'parents': [{'id': '1niAms7QddkNxn8NU3b6AKHViA-efVJfa'}]})
+f.SetContentFile('rates_result.csv')
+f.Upload() # Upload the file.
+
