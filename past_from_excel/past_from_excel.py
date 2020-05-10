6a6757ebd25945b6b613e9156775a496ac4925cd
unknown <tadej.ostanek@spitogatos.gr>
2020-05-10 23:28:48 +0300

    import from past excel to have something to start to work with

---
+++ past_from_excel/past_from_excel.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 10 22:53:04 2020
+
+@author: tadej
+"""
+
+from pathlib import Path
+import pandas as pd
+import os
+
+for root, dirs, files in os.walk(Path("past_from_excel")):
+    for file in files:
+        if not file.endswith('.py'):
+            datest = file[:file.index('.csv')]
+            data = pd.read_csv(Path('past_from_excel', file),
+                               parse_dates=['Date'])
+            data = data.assign(Date=data.Date.dt.date)
+
+            data = data.melt('Date')
+            data = data.rename(columns={'Date': 'arrivalDate',
+                                        'variable': 'hotelName'})
+            data.insert(0, 'date_stamp', datest)
+            data = data.assign(value=data.value.replace(
+                'Sold out', '0').astype(float))
+
+            data.to_csv(Path('data_export/rates_result/' + datest + '.csv'),
+                        index=False)
