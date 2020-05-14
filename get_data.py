b24ab8fb52a19444d310a6e8c65b8bcdc22f630c
unknown <tadej.ostanek@spitogatos.gr>
2020-05-14 23:11:34 +0300

    small modifications

--- get_data.py
+++ get_data.py
@@ -19,7 +19,7 @@ client = OTAInsight(token)
 rates_data = client.get_rates(sub_id=str(kiani_subid), los='3',
                               from_date='2020-03-01', shop_length='250')
 
-rates_data['date_stamp'] = date_stamp
+rates_data['date_stamp'] = str(date_stamp)
 
 rates_data = rates_data.loc[:, ['date_stamp', 'arrivalDate', 'hotelName',
                             'value']]
--- google_drive.py
+++ google_drive.py
@@ -13,6 +13,6 @@ gauth.LocalWebserverAuth()
 
 drive = GoogleDrive(gauth)
 f = drive.CreateFile({'parents': [{'id': '1niAms7QddkNxn8NU3b6AKHViA-efVJfa'}]})
-f.SetContentFile('rates_result.csv')
+f.SetContentFile('all_data.csv')
 f.Upload() # Upload the file.
 
