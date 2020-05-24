8802c600847225cbf5eee44883fbf3501c102503
unknown <tadej.ostanek@spitogatos.gr>
2020-05-24 20:37:13 +0300

    added two sites to take data from + import of all old data in easier format

--- get_data.py
+++ get_data.py
@@ -6,6 +6,8 @@ Created on Sun Apr 12 14:46:16 2020
 """
 from ota import OTAInsight
 from datetime import date
+import pandas as pd
+import numpy as np
 
 kiani_subid = 89365
 
@@ -16,13 +18,20 @@ with open('auth/ota_token.txt', 'r') as fa:
 
 client = OTAInsight(token)
 
-rates_data = client.get_rates(sub_id=str(kiani_subid), los='3',
-                              from_date='2020-03-01', shop_length='250')
+plist = dict()
+for site in ['bookingdotcom', 'expedia']:
+    plist[site] = client.get_rates(
+        sub_id=str(kiani_subid), los='3', ota=site,
+        from_date='2020-03-01', shop_length='250')
 
+rates_data = pd.concat(plist).reset_index().rename(
+    columns={'level_0': 'site'}).drop(columns='level_1')
 rates_data['date_stamp'] = str(date_stamp)
 
-rates_data = rates_data.loc[:, ['date_stamp', 'arrivalDate', 'hotelName',
-                            'value']]
+rates_data = rates_data.loc[:, ['date_stamp', 'site', 'arrivalDate',
+                                'hotelName', 'value']]
+
+rates_data = rates_data.assign(value=rates_data.value.replace(0, np.nan))
 
 rates_data.to_csv('data_export/rates_result' + str(date_stamp) + '.csv',
                   index=False)
--- past_from_excel/past_from_excel.py
+++ past_from_excel/past_from_excel.py
@@ -7,22 +7,30 @@ Created on Sun May 10 22:53:04 2020
 
 from pathlib import Path
 import pandas as pd
+import numpy as np
 import os
 
 for root, dirs, files in os.walk(Path("past_from_excel")):
     for file in files:
-        if not file.endswith('.py'):
-            datest = file[:file.index('.csv')]
+        if file.endswith('.csv'):
+            site = file[:file.index('_'):]
+            hotel = file[file.index('_') + 1:file.index('.csv')]
             data = pd.read_csv(Path('past_from_excel', file),
                                parse_dates=['Date'], dayfirst=True)
             data = data.assign(Date=data.Date.dt.date)
 
             data = data.melt('Date')
             data = data.rename(columns={'Date': 'arrivalDate',
-                                        'variable': 'hotelName'})
-            data.insert(0, 'date_stamp', datest)
-            data = data.assign(value=data.value.replace(
-                'Sold out', '0').astype(float))
+                                        'variable': 'date_stamp'})
+            data.insert(2, 'hotelName', hotel)
+            data.insert(2, 'site', site)
+            # drop all snapshots in the future
+            data = data.loc[~data.value.isna()]
 
-            data.to_csv(Path('data_export/rates_result' + datest + '.csv'),
-                        index=False)
+            data = data.assign(value=data.value.replace(
+                'Sold out', np.nan).replace('No data', np.nan).astype(float))
+
+            data.to_csv(Path(
+                'data_export/rates_result_past_' +
+                site + '_' + hotel + '.csv'),
+                index=False)
