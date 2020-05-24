d3dcbb32430bfb50e9de67be6bc0d70487919252
unknown <tadej.ostanek@spitogatos.gr>
2020-05-24 21:33:16 +0300

    rework google drive upload to overwrite

--- google_drive.py
+++ google_drive.py
@@ -8,20 +8,27 @@ from datetime import date
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
+
 gauth = GoogleAuth()
 gauth.LocalWebserverAuth()
 
 drive = GoogleDrive(gauth)
 
-# upload all data
+def overwrite_upload(folderid, file_name, path=None)
+    file_list = drive.ListFile(
+        {'q': f"'{folderid}' in parents and trashed = False"}).GetList()
+    for file in file_list:
+        if file['title'] == file_name:
+            file.Delete()
+    f = drive.CreateFile({'parents': [{'id': folderid}]})
+    if path is not None:
+        file_path = path + '/' + file_name
+    else:
+        file_path = file_name
+    f.SetContentFile(file_path)
+    f.Upload()
 
-f = drive.CreateFile({
-    'parents': [{'id': '1niAms7QddkNxn8NU3b6AKHViA-efVJfa'}]})
-f.SetContentFile('all_data.csv')
-f.Upload() # Upload the file.
-
-# upload the raw file
-f = drive.CreateFile({
-    'parents': [{'id': '131uqtvxWy0qIzg4Fs0wIszeM09B7ZJ07'}]})
-f.SetContentFile('data_export/rates_result' + str(date.today()) + '.csv')
-f.Upload() # Upload the file.
+overwrite_upload('1niAms7QddkNxn8NU3b6AKHViA-efVJfa', 'all_data.csv')
+overwrite_upload('131uqtvxWy0qIzg4Fs0wIszeM09B7ZJ07',
+                 'rates_result' + str(date.today()) + '.csv',
+                 path='data_export')
