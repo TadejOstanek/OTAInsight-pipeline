588e3cca54ac31a496abdefdddd45d9c1892234d
unknown <tadej.ostanek@spitogatos.gr>
2020-05-24 20:58:18 +0300

    upload rawdata to google drive too

--- google_drive.py
+++ google_drive.py
@@ -4,7 +4,7 @@ Created on Sun May  3 15:29:52 2020
 connect to google drive
 @author: tadej
 """
-
+from datetime import date
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
@@ -12,7 +12,17 @@ gauth = GoogleAuth()
 gauth.LocalWebserverAuth()
 
 drive = GoogleDrive(gauth)
-f = drive.CreateFile({'parents': [{'id': '1niAms7QddkNxn8NU3b6AKHViA-efVJfa'}]})
+
+# upload all data
+
+f = drive.CreateFile({
+    'parents': [{'id': '1niAms7QddkNxn8NU3b6AKHViA-efVJfa'}]})
 f.SetContentFile('all_data.csv')
 f.Upload() # Upload the file.
 
+# upload the raw file
+f = drive.CreateFile({
+    'parents': [{'id': '131uqtvxWy0qIzg4Fs0wIszeM09B7ZJ07'}]})
+f.SetContentFile('data_export/rates_result' + str(date.today()) + '.csv')
+f.Upload() # Upload the file.
+rates_data.to_csv('data_export/rates_result' + str(date_stamp) + '.csv')
