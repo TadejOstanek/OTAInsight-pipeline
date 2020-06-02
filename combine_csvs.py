24a80f3d10dcb6aaee341da48bde45824d233681
unknown <tadej.ostanek@spitogatos.gr>
2020-06-02 12:30:10 +0300

    only keep daily data of last week

--- combine_csvs.py
+++ combine_csvs.py
@@ -7,12 +7,24 @@ Created on Sun May 10 23:41:35 2020
 
 import os
 from pathlib import Path
+import re
+from datetime import datetime, date
 import pandas as pd
 
 dfs = list()
 for root, folder, files in os.walk(Path('data_export')):
     for file in files:
-        dfs.append(pd.read_csv('data_export/' + file))
+        try:
+            date_parse = datetime.strptime(
+                re.search(r'\d\d\d\d-\d\d-\d\d', file)[0], '%Y-%m-%d').date()
+        # no date in file name
+        except TypeError:
+            day_diff = 0
+        else:
+            day_diff = (date.today() - date_parse).days
+
+        if day_diff <= 7 or day_diff % 7 == 0:
+            dfs.append(pd.read_csv('data_export/' + file))
 
 all_data = pd.concat(dfs)
 
