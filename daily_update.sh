61daf46854fab02fc335451cd64b869494e06e59
unknown <tadej.ostanek@spitogatos.gr>
2020-11-14 18:46:06 +0200

    reorganization

--- daily_update.sh
+++ daily_update.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 cd /home/tadej_ostanek/cretehotels
 ~/miniconda3/envs/ota-insight/bin/python '/home/tadej_ostanek/cretehotels/__main__.py'
 ~/miniconda3/envs/ota-insight/bin/python '/home/tadej_ostanek/cretehotels/google_drive.py'
