f7839e090bb4575eb1e5472621c3dfba1ae31ac6
unknown <tadej.ostanek@spitogatos.gr>
2020-11-15 23:59:25 +0200

    move through directories

--- daily_update.sh
+++ daily_update.sh
@@ -1,3 +1,4 @@
-cd /home/tadej_ostanek/cretehotels
+cd /home/tadej_ostanek/cretehotels/src
 ~/miniconda3/envs/ota-insight/bin/python '/home/tadej_ostanek/cretehotels/src/__main__.py'
+cd /home/tadej_ostanek/cretehotels
 ~/miniconda3/envs/ota-insight/bin/python '/home/tadej_ostanek/cretehotels/google_drive.py'
