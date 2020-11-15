807562d555ffc91ce41e8f7aaeea02362705f391
unknown <tadej.ostanek@spitogatos.gr>
2020-11-15 23:57:40 +0200

    src in sh

--- daily_update.sh
+++ daily_update.sh
@@ -1,3 +1,3 @@
 cd /home/tadej_ostanek/cretehotels
-~/miniconda3/envs/ota-insight/bin/python '/home/tadej_ostanek/cretehotels/__main__.py'
+~/miniconda3/envs/ota-insight/bin/python '/home/tadej_ostanek/cretehotels/src/__main__.py'
 ~/miniconda3/envs/ota-insight/bin/python '/home/tadej_ostanek/cretehotels/google_drive.py'
